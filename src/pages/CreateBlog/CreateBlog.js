45e5c8b1b37937b1d1bee5ec493be2f0e60cc28b
ademulic <ademulic@hotmail.com>
2025-03-04 17:36:29 +0100

    ODradjeno puno

--- src/pages/CreateBlog/CreateBlog.js
+++ src/pages/CreateBlog/CreateBlog.js
@@ -1,7 +1,21 @@
+import './CreateBlog.css';
+
 const CreateBlog = () => {
     return ( 
-        <section>
-            CreateBlog component
+        <section className="create-blog">
+            <form>
+                <h2>Create a new Blog</h2> 
+                <div>
+                    <label htmlFor="blog-title"><p>Title:</p></label>
+                    <input id="blog-title" type="text" placeholder="Enter Blog title..." /> 
+                </div>
+                 
+                <label htmlFor="blog-body"><p></p>Body</label>
+                <textarea id="blog-body" placeholder='Enter blog body...'></textarea>
+                
+                <button>Submit</button>
+            </form>            
+            
         </section>
      );
 }
