65e51fe7c68f708521e284b0efed59bb5cc4f819
Your Nameademulic <ademulic@hotmail.com>
2025-03-05 15:04:04 +0100

    male izmene unete

--- src/pages/CreateBlog/CreateBlog.js
+++ src/pages/CreateBlog/CreateBlog.js
@@ -5,11 +5,15 @@ const CreateBlog = () => {
         <section className="create-blog">
             <form>
                 <h2>Create a new Blog</h2> 
+                <p style={{textAlign:'center'}}>Please fill out the form below</p>
                 <div>
                     <label htmlFor="blog-title"><p>Title:</p></label>
                     <input id="blog-title" type="text" placeholder="Enter Blog title..." /> 
                 </div>
-                 
+                <div>
+                    <label htmlFor="blog-title"><p>Image URl:</p></label>
+                    <input id="blog-title" type="text" placeholder="Paste or type an image url from the internet..." /> 
+                </div>
                 <label htmlFor="blog-body"><p></p>Body</label>
                 <textarea id="blog-body" placeholder='Enter blog body...'></textarea>
                 
