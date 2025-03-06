407333902d6c999c009e02d3b74409c3d8975b71
Your Nameademulic <ademulic@hotmail.com>
2025-03-06 10:02:43 +0100

    Finished the project for now

--- src/pages/BlogDetails/BlogDetails.js
+++ src/pages/BlogDetails/BlogDetails.js
@@ -1,11 +1,24 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './BlogDetails.css';
 import useFetch from '../../Utils/useFetch';
 import { BounceLoader } from "react-spinners";
+import { useState } from 'react';
 const BlogDetails = () => {
     
     const {id} = useParams()
     const {data:blog, loading, error} = useFetch('http://localhost:8000/blogs/'+ id);
+    const navigate = useNavigate();
+    const [isLoading,setIsLoading]= useState(false);
+    const handleDelete = ()=>{
+        setTimeout(()=>{
+            fetch(`http://localhost:8000/blogs/${id}`,{method:"DELETE"})
+            .then(()=>{
+                setIsLoading(true);
+                navigate('/');
+            })
+        },1000)
+        
+    }
 
     if(loading)
         return (
@@ -25,13 +38,26 @@ const BlogDetails = () => {
         
         <section className="blog-details"> 
             {error && <p>{error}</p>}
-            {blog && <div>
+            {blog && 
+            <article>
                 <h1>{blog.title}</h1>
                 <p className="blog-details-author">Author: {blog.author? String(blog.author):"Unknown"}</p>
                 <p className="blog-details-body">
                     {blog.body}
                 </p>
-            </div>} 
+            </article>} 
+            {!isLoading ? 
+                <button className='delete-btn' onClick={handleDelete}>Delete this blog</button> :
+                <span>
+                    <BounceLoader
+                    color="orange"
+                    cssOverride={{}}
+                    loading
+                    size={60}
+                    />
+                    <p>Deleting Blog...</p> 
+                </span>
+            }
         </section>
      );
 }
--- src/pages/CreateBlog/CreateBlog.js
+++ src/pages/CreateBlog/CreateBlog.js
@@ -1,25 +1,108 @@
-import './CreateBlog.css';
-
+import { useEffect,useState } from 'react';
+import './CreateBlog.css'; 
+import { BounceLoader } from "react-spinners";
+import { useNavigate } from 'react-router-dom';
 const CreateBlog = () => {
+    const [users, setUsers] = useState(null);
+    const [title, setTitle] = useState(''); 
+    const [thumbnail, setThumbnail] = useState('');
+    const [author, setAuthor] = useState('Ryu');
+    const [body,setBody] = useState('');
+    const [loading, setLoading]= useState(false);
+    const navigate = useNavigate();
+    useEffect(()=>{
+        fetch('http://localhost:8000/users')
+        .then(res=>{
+            if(!res.ok){
+                throw Error("there was a problem with the users database, sorry :(")
+            }else
+            {
+                return res.json()
+            }
+        })
+        .then(data=>setUsers(data)).catch((err)=>{console.log(err.message)});
+    },[]);
+
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const blog = {title,thumbnail,author,body};
+        setLoading(true);
+        setTimeout(()=>{
+            fetch('http://localhost:8000/blogs', {
+                method:"POST",
+                headers: {"Content-Type":"application/json"},
+                body:JSON.stringify(blog)
+            }).then(()=>{
+                navigate('/'); 
+                setLoading(false);
+            })
+        },1000)
+        
+    }
+
+
     return ( 
         <section className="create-blog">
-            <form>
+
+            {/* <p>{title}</p>
+            <p>{imgUrl}</p>
+            <p>{body}</p>
+            <p>{author}</p> */}
+            <form onSubmit={handleSubmit}>
                 <h2>Create a new Blog</h2> 
                 <p style={{textAlign:'center'}}>Please fill out the form below</p>
                 <div>
                     <label htmlFor="blog-title"><p>Title:</p></label>
-                    <input id="blog-title" type="text" placeholder="Enter Blog title..." /> 
+                    <input 
+                        id="blog-title" 
+                        type="text" 
+                        placeholder="Enter Blog title..." 
+                        value={title} 
+                        onChange={(e)=>setTitle(e.target.value)}
+                    /> 
                 </div>
                 <div>
-                    <label htmlFor="blog-title"><p>Image URl:</p></label>
-                    <input id="blog-title" type="text" placeholder="Paste or type an image url from the internet..." /> 
+                    <label htmlFor="img-url"><p>Image URl:</p></label>
+                    <input 
+                        id="img-url" 
+                        type="text" 
+                        placeholder="Paste or type an image url from the internet..." 
+                        value={thumbnail}
+                        onChange={(e)=>setThumbnail(e.target.value)}
+                    /> 
                 </div>
+                <select 
+                    value={author}
+                    onChange={(e)=>setAuthor(e.target.value)}>
+                    {
+                        users && users.map((user)=>
+                            <option value={user.name} key={user.id} >{user.name}</option>
+                        )
+                    }
+                </select>
                 <label htmlFor="blog-body"><p></p>Body</label>
-                <textarea id="blog-body" placeholder='Enter blog body...'></textarea>
+                <textarea 
+                    id="blog-body" 
+                    placeholder='Enter blog body...' 
+                    value={body}
+                    onChange={(e)=>setBody(e.target.value)}
+                ></textarea>
                 
-                <button type='submit'>Submit</button>
+                {!loading ? <button type='submit'>Submit</button>:''}
+                {loading &&  
+                    <span>
+                        <BounceLoader
+                        color="orange"
+                        cssOverride={{}}
+                        loading
+                        size={60}
+                        /><p>Adding Blog...</p>
+                    </span>
+                }
             </form>            
             
+
         </section>
      );
 }
